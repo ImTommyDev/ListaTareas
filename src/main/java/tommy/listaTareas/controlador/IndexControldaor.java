349bf83e0389ee59b9cc7f876cc21b8a6a3fe9a8
ImTommyDev <tommy.devlpr@gmail.com>
2024-11-01 14:59:12 +0100

    Se han añadido las dependencias para que se pueda usar java fx y se ha creado el controlador.

---
+++ src/main/java/tommy/listaTareas/controlador/IndexControldaor.java
@@ -0,0 +1,14 @@
+package tommy.listaTareas.controlador;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class IndexControldaor implements Initializable {
+    //Método para que se pueda iniciar la app
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
